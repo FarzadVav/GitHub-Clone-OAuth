415adbf66cec28ce5d09a4f3298c2f871daaf251
FarzadVav <farzad.vav.work@gmail.com>
2024-09-09 13:10:39 +0330

    update: get user repositories and show in /repos page

--- app/repos/page.tsx
+++ app/repos/page.tsx
@@ -1,5 +1,39 @@
-const Repos = () => {
-  return <div>Repos</div>
+import { verifySession } from "@/lib/sessions"
+import Repository from "@/lib/types/repository.types"
+
+const getRepositories = async () => {
+  const session = await verifySession()
+  if (!session) {
+    return null
+  }
+
+  const repositoriesResponse = await fetch("https://api.github.com/user/repos", {
+    headers: {
+      Authorization: `Bearer ${session?.access_token}`,
+      Accept: "application/vnd.github+json",
+      "X-GitHub-Api-Version": "2022-11-28",
+    },
+  })
+  if (!repositoriesResponse.ok) {
+    return null
+  }
+
+  const responseResult = (await repositoriesResponse.json()) as Repository[]
+  return responseResult
+}
+
+const Repos = async () => {
+  const repositories = await getRepositories()
+  // @ts-ignore
+  console.log("repositories --------------------->", repositories[0])
+
+  return (
+    <div>
+      {repositories?.map((repo) => (
+        <div>{repo.name}</div>
+      ))}
+    </div>
+  )
 }
 
 export default Repos
---
+++ lib/types/repository.types.ts
@@ -0,0 +1,9 @@
+type Repository = {
+  name: string
+  full_name: string
+  description: string
+  url: string
+  private: boolean
+}
+
+export default Repository
