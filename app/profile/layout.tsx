1d287526916d519db6bea5422cdfc65f080ff59a
FarzadVav <farzad.vav.work@gmail.com>
2024-09-09 16:32:29 +0330

    add: ProfileSideBar component and use in /profile page

--- app/profile/layout.tsx
+++ app/profile/layout.tsx
@@ -1,18 +1,12 @@
 import React from "react"
-import { Bars3Icon } from "@heroicons/react/24/outline"
 
 import ProfileHeader from "@/components/modules/profile/ProfileHeader"
+import ProfileSideBar from "@/components/modules/profile/ProfileSideBar"
 
 const ProfileLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   return (
     <div className="w-screen h-screen flex">
-      <div className="w-16 h-full">
-        <div className="size-16 flex justify-center items-center">
-          <button className="btn btn-square">
-            <Bars3Icon className="size-4" />
-          </button>
-        </div>
-      </div>
+      <ProfileSideBar />
       <div className=" h-full w-[calc(100%-4rem)]">
         <ProfileHeader />
         <div className="bg-base-200 w-full min-h-[calc(100%-4rem)] p-6 rounded-tl-lg overflow-y-auto">
---
+++ components/modules/profile/ProfileSideBar.tsx
@@ -0,0 +1,44 @@
+import {
+  AcademicCapIcon,
+  AdjustmentsHorizontalIcon,
+  Bars3Icon,
+  BoldIcon,
+  BriefcaseIcon,
+  CameraIcon,
+  HomeIcon,
+  SpeakerWaveIcon,
+} from "@heroicons/react/24/outline"
+
+const links = [
+  { icon: <HomeIcon className="size-4" />, name: "Home" },
+  { icon: <BriefcaseIcon className="size-4" />, name: "Briefcase" },
+  { icon: <SpeakerWaveIcon className="size-4" />, name: "Speaker" },
+  { icon: <AcademicCapIcon className="size-4" />, name: "Educations" },
+  { icon: <BoldIcon className="size-4" />, name: "Energy" },
+  { icon: <CameraIcon className="size-4" />, name: "Camera" },
+  { icon: <AdjustmentsHorizontalIcon className="size-4" />, name: "Filters" },
+]
+
+const ProfileSideBar = () => {
+  return (
+    <div className="w-16 h-full">
+      <div className="size-16 flex justify-center items-center">
+        <button className="btn btn-square" aria-label="Menu button">
+          <Bars3Icon className="size-4" />
+        </button>
+      </div>
+      <ul className="w-full h-[calc(100%-5.5rem)] mt-6 overflow-y-auto">
+        <div className="bg-base-200 ring-1 ring-base-300 size-12 mx-auto mt-[1px] rounded-full"></div>
+        {links.map((link) => (
+          <li key={link.name} className="mx-auto w-max mt-1 first-of-type:mt-2">
+            <button className="btn btn-square btn-ghost" title={link.name} aria-label={link.name}>
+              {link.icon}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default ProfileSideBar
